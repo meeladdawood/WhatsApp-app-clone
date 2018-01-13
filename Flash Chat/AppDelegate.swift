560502a32fd2474b4c71c46f7ecdadb9d3b35975
Meelad Dawood <mdawood@sandiego.edu>
2018-01-13 14:46:29 -0800

    Fixed the historical messages bug where old messages were not displayed.

--- Flash Chat/AppDelegate.swift
+++ Flash Chat/AppDelegate.swift
@@ -21,8 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //TODO: Initialise and Configure your Firebase here:
         FirebaseApp.configure()
         
-        let myDatabase = Database.database().reference()
-        myDatabase.setValue("We have data!!!")
+    
+       
         
         
         return true
